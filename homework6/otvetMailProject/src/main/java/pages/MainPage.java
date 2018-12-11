ea319836a0eb34a6afcc126e825765b740fe43f9
Maxim Yaremchuk <myaremchuk@yandex.ru>
2018-12-11 21:44:41 +0300

    homework5-6

--- homework6/otvetMailProject/src/main/java/pages/MainPage.java
+++ homework6/otvetMailProject/src/main/java/pages/MainPage.java
@@ -4,10 +4,14 @@ import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
 import io.qameta.allure.Step;
+import io.restassured.response.Response;
 import navigation.DefaultUrl;
+
+import java.util.HashMap;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
+import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertTrue;
 
 @DefaultUrl("/open/")
@@ -19,10 +23,16 @@ public class MainPage extends PageObject<MainPage> {
     @Step("Кликаем на аватарку пользователя")
     public MainPage pressUserAvatar(){
         userId = avatarCollection.get(0).attr("data-user");
-        System.out.println(avatarCollection.get(0).attr("style"));
         avatarCollection.get(0).click();
-        assertTrue(WebDriverRunner.url().contains(userId));
+        assertTrue("проверям что открылась страница нужного пользователя", WebDriverRunner.url().contains(userId));
+        assertTrue("проверяем что страница открылась без ошибок", getCodeHttpRequest() == 200);
         return this;
     }
+    private int getCodeHttpRequest() {
+        Response response = given().relaxedHTTPSValidation().when().get(WebDriverRunner.url(), new HashMap<>());
+        return response.getStatusCode();
+    }
+
+
 
 }
--- homework6/otvetMailProject/src/test/java/clickUserAvatar_Test.java
+++ homework6/otvetMailProject/src/test/java/clickUserAvatar_Test.java
@@ -2,7 +2,7 @@ import io.qameta.allure.junit4.DisplayName;
 import org.junit.Test;
 import pages.MainPage;
 
-public class pagination_Test {
+public class clickUserAvatar_Test {
     @Test
     @DisplayName("Проверка открытия профиля пользователя")
     public void checkPaginationView() {
